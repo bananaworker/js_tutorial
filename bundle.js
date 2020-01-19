3392025b813dd67ccd0fcc3f66494b25dacc5637
Muzafar Ahmed <ahmed.muzafar@gmail.com>
2020-01-19 16:00:24 +0100

    update JS_tutorial with latest palindrom version

--- bundle.js
+++ bundle.js
@@ -1,16 +1,25 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("muz-palindrome");
 
-let string = prompt("Go ahead, enter a string for a palindrome check.");
-let phrase = new Phrase(string)
+function palindromeTester(event) {
+  event.preventDefault();
+  // let string = prompt("Go ahead, enter a string for a palindrome check.");
+  let phrase = new Phrase(event.target.phrase.value)
+  let palindromeResult = document.querySelector("#palindromeResult")
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content} is a palinfrome! Now go outside."`)
-} else {
-  alert("That aint no gawd damn palindrome fool.. Now go outside")
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = (`"<strong>${phrase.content}</strong>" is a palindrome!`)
+  } else {
+    palindromeResult.innerHTML = ("That aint no gawd damn palindrome fool.. Now go outside")
+  }
 }
 
-alert(string);
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+      palindromeTester(event);
+  });
+});
 
 },{"muz-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
@@ -31,13 +40,17 @@ function Phrase (content) {
 
   // Returns letters only from string.
   this.letters = function letters() {
-    let lettersRegex = /[a-z]/ig
+    const lettersRegex = /[a-z]/ig;
     return ((this.content).match(lettersRegex) || []).join("");
   }
 
   // Returns true for palindrome, false otherwise
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.processedContent()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
 
 }
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 let Phrase = require("muz-palindrome");
 
-let string = prompt("Go ahead, enter a string for a palindrome check.");
-let phrase = new Phrase(string)
+function palindromeTester(event) {
+  event.preventDefault();
+  // let string = prompt("Go ahead, enter a string for a palindrome check.");
+  let phrase = new Phrase(event.target.phrase.value)
+  let palindromeResult = document.querySelector("#palindromeResult")
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content} is a palinfrome! Now go outside."`)
-} else {
-  alert("That aint no gawd damn palindrome fool.. Now go outside")
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = (`"<strong>${phrase.content}</strong>" is a palindrome!`)
+  } else {
+    palindromeResult.innerHTML = ("That aint no gawd damn palindrome fool.. Now go outside")
+  }
 }
 
-alert(string);
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+      palindromeTester(event);
+  });
+});
